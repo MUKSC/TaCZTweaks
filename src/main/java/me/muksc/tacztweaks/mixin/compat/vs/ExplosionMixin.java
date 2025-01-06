bf32b47e254767e7d2a8b2fb143705deac73a9fb
MUKSC <68376185+MUKSC@users.noreply.github.com>
2025-01-06 23:55:00 +0900

    what happened

--- src/main/java/me/muksc/tacztweaks/mixin/compat/vs/ExplosionMixin.java
+++ src/main/java/me/muksc/tacztweaks/mixin/compat/vs/ExplosionMixin.java
@@ -2,11 +2,15 @@ package me.muksc.tacztweaks.mixin.compat.vs;
 
 import me.muksc.tacztweaks.compat.vs.ExplosionInvoker;
 import net.minecraft.world.level.Explosion;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(value = Explosion.class, remap = false)
+@Mixin(value = Explosion.class, priority = 1500, remap = false)
 public abstract class ExplosionMixin implements ExplosionInvoker {
-    @SuppressWarnings("MissingUnique")
+    @SuppressWarnings("target")
+    @Dynamic
+    @Shadow
     private void doExplodeForce() {
         throw new AssertionError();
     }
