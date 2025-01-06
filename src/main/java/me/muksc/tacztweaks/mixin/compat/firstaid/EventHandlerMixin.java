8852312b5c93a2ac6cc560e9fd07d8f2e9e1ac4e
MUKSC <68376185+MUKSC@users.noreply.github.com>
2025-01-07 00:27:43 +0900

    next time, test everything before releasing ok?

--- src/main/java/me/muksc/tacztweaks/mixin/compat/firstaid/EventHandlerMixin.java
+++ src/main/java/me/muksc/tacztweaks/mixin/compat/firstaid/EventHandlerMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = EventHandler.class, remap = false)
 public abstract class EventHandlerMixin {
-    @ModifyExpressionValue(method = "onLivingHurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/damagesource/DamageSource;is(Lnet/minecraft/tags/TagKey;)Z"))
+    @ModifyExpressionValue(method = "onLivingHurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/damagesource/DamageSource;is(Lnet/minecraft/tags/TagKey;)Z", remap = true))
     private static boolean bulletsAreProjectile(boolean original, @Local DamageSource source) {
         if (!Config.firstAidCompat) return original;
         return original || source.is(ModDamageTypes.BULLETS_TAG);
