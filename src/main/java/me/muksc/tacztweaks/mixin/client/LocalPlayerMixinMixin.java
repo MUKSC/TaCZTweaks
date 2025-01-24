7531c18d40e8ad7ef67a0733caf0d4478534cfb5
MUKSC <68376185+MUKSC@users.noreply.github.com>
2025-01-24 10:19:13 +0900

    `sprintWhileReloading` for weapons with dynamic reload

--- src/main/java/me/muksc/tacztweaks/mixin/client/LocalPlayerMixinMixin.java
+++ src/main/java/me/muksc/tacztweaks/mixin/client/LocalPlayerMixinMixin.java
@@ -2,7 +2,9 @@ package me.muksc.tacztweaks.mixin.client;
 
 import com.bawnorton.mixinsquared.TargetHandler;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.tacz.guns.client.gameplay.LocalPlayerCrawl;
+import com.tacz.guns.client.gameplay.LocalPlayerReload;
 import me.muksc.tacztweaks.Config;
 import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Dynamic;
@@ -11,6 +13,16 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = LocalPlayer.class, priority = 1500, remap = false)
 public abstract class LocalPlayerMixinMixin {
+    @Dynamic
+    @TargetHandler(
+        mixin = "com.tacz.guns.mixin.client.LocalPlayerMixin",
+        name = "swapSprintStatus"
+    )
+    @WrapWithCondition(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lcom/tacz/guns/client/gameplay/LocalPlayerReload;cancelReload()V"))
+    private boolean sprintWhileReloading(LocalPlayerReload instance, @Local(argsOnly = true) boolean sprinting) {
+        return sprinting && !Config.sprintWhileReloading;
+    }
+
     @Dynamic
     @TargetHandler(
         mixin = "com.tacz.guns.mixin.client.LocalPlayerMixin",
