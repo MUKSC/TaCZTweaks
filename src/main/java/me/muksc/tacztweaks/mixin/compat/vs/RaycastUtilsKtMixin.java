47131756abd0759704f58a445da9774d1a05cbbc
MUKSC <68376185+MUKSC@users.noreply.github.com>
2025-03-29 10:47:13 +0900

    use correct vs version

--- src/main/java/me/muksc/tacztweaks/mixin/compat/vs/RaycastUtilsKtMixin.java
+++ src/main/java/me/muksc/tacztweaks/mixin/compat/vs/RaycastUtilsKtMixin.java
@@ -6,13 +6,13 @@ import me.muksc.tacztweaks.compat.vs.BlockHitResultExtension;
 import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.core.api.ships.LoadedShip;
 import org.valkyrienskies.mod.common.world.RaycastUtilsKt;
 
 @Mixin(value = RaycastUtilsKt.class, remap = false)
 public abstract class RaycastUtilsKtMixin {
     @ModifyExpressionValue(method = "clipIncludeShips(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/ClipContext;ZLjava/lang/Long;)Lnet/minecraft/world/phys/BlockHitResult;", at = @At(value = "INVOKE", target = "Lorg/valkyrienskies/mod/common/world/RaycastUtilsKt;clip(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/ClipContext;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;)Lnet/minecraft/world/phys/BlockHitResult;"))
-    private static BlockHitResult tacztweaks$clipIncludeShips$setShip(BlockHitResult original, @Local Ship ship) {
+    private static BlockHitResult tacztweaks$clipIncludeShips$setShip(BlockHitResult original, @Local LoadedShip ship) {
         BlockHitResultExtension ext = (BlockHitResultExtension) original;
         ext.tacztweaks$setShip(ship);
         return original;
